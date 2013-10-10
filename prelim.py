957602b4057b9e5cd79b083def18898366637d61
Kemal Eren <kemal@kemaleren.com>
2013-10-10 16:01:55 -0700

    watershed for seperating connected components

--- prelim.py
+++ prelim.py
@@ -1,14 +1,18 @@
 from __future__ import division
 
 import glymur
-import sklearn.decomposition as decomp
-from skimage.filter import threshold_otsu
-from skimage.morphology import label
-from skimage.color import label2rgb
-from scipy.stats import poisson
+
 import numpy as np
 
-from threshold import poisson_threshold
+from scipy.stats import poisson
+from scipy import ndimage
+
+import sklearn.decomposition as decomp
+
+from skimage.filter import threshold_otsu
+from skimage.color import label2rgb
+from skimage.morphology import watershed
+from skimage.feature import peak_local_max
 
 img = glymur.Jp2k('../data/images/example.jp2')
 img = img.read(rlevel=1)
@@ -16,8 +20,11 @@ img = img[4000:6000, 3000:img.shape[1] / 2, :]  # approximately symmetric
 
 pca = decomp.PCA(1)
 img = pca.fit_transform(img.reshape(-1, 3)).reshape(img.shape[:2])
-img = ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
-threshold = poisson_threshold(img)
-labels = label(img < threshold, background=0)
-labeled_img = label2rgb(labels, img, bg_label=0)
+img = (img - img.min()) / (img.max() - img.min())
 
+binary = img < threshold_otsu(img)
+distance = ndimage.distance_transform_edt(binary)
+local_maxi = peak_local_max(distance, indices=False, footprint=np.ones((3, 3)), labels=binary)
+markers = ndimage.label(local_maxi)[0]
+labels = watershed(-distance, markers, mask=binary)
+labeled_img = label2rgb(labels, img, bg_label=0)
