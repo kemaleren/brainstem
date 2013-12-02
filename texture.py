06e4b1fdaef6e5122fce3c838f3d269ae9110157
Kemal Eren <kemal@kemaleren.com>
2013-12-02 13:26:24 -0800

    making texture functions more modular

--- texture.py
+++ texture.py
@@ -25,29 +25,44 @@ def make_filter_bank(frequencies, thetas, real=True):
     """prepare filter bank of kernels"""
     # TODO: set MTF of each filter at (u, v) to 0
     kernels = []
-    all_freqs = []
+    kernel_freqs = []
     for frequency in frequencies:
         sigma_x, sigma_y = _compute_sigmas(frequency)
         for theta in thetas:
             kernel = gabor_kernel(frequency, theta=theta,
                                   bandwidth=1)
             kernels.append(kernel)
-            all_freqs.append(frequency)
+            kernel_freqs.append(frequency)
     if real:
         kernels = list(np.real(k) for k in kernels)
-    return kernels, np.array(all_freqs)
+    return kernels, np.array(kernel_freqs)
 
 
-def filter_image(image, kernels, frequencies, crop=True, select=True, r2=0.95):
-    """Computes all convolutions and discards some filtered images.
+def get_freqs(img):
+    """Compute the appropriate frequencies for an image of the given shape.
 
-    Returns filtered images with the largest energies so that the
-    coefficient of determiniation is >= ``r2``.
+    Frequencies are given in cycles/pixel.
 
     """
-    filtered = np.dstack(fftconvolve(image, kernel, 'same')
-                         for kernel in kernels)
+    n_cols = img.shape[1]
+    next_pow2 = 2 ** int(np.ceil(np.log2(n_cols)))
+    min_freq = next_pow2 / 4
+    n_freqs = int(np.log2(min_freq))
 
+    # note: paper gives frequency in cycles per image width.
+    # we need cycles per pixel, so divide by image width
+    frequencies =  list((np.sqrt(2) * float(2 ** i)) / n_cols
+                        for i in range(n_freqs))
+    return frequencies
+
+
+def filter_img(img, freqs=None, angle=10, crop=True):
+    if freqs is None:
+        freqs = get_freqs(img)[-4:]
+    thetas = np.deg2rad(np.arange(0, 180, angle))
+    kernels, kernel_freqs = make_filter_bank(freqs, thetas)
+    filtered = np.dstack(fftconvolve(img, kernel, 'same')
+                         for kernel in kernels)
     if crop:
         x = max(k.shape[0] for k in kernels)
         y = max(k.shape[1] for k in kernels)
@@ -55,8 +70,16 @@ def filter_image(image, kernels, frequencies, crop=True, select=True, r2=0.95):
         y = int(np.ceil(y / 2))
         filtered = filtered[x:-x, y:-y]
 
-    if not select:
-        return filtered, frequencies
+    return filtered, kernel_freqs
+
+
+def filter_selection(filtered, kernel_freqs, r2=0.95):
+    """Discards some filtered images.
+
+    Returns filtered images with the largest energies so that the
+    coefficient of determiniation is >= ``r2``.
+
+    """
     energies = filtered.sum(axis=0).sum(axis=0)
 
     # sort from largest to smallest energy
@@ -69,6 +92,7 @@ def filter_image(image, kernels, frequencies, crop=True, select=True, r2=0.95):
     k = np.searchsorted(r2s, r2)
     n_start = filtered.shape[2]
     return filtered[:, :, :k], frequencies[idx][:k]
+
 
 
 def compute_features(filtered, frequencies,
@@ -111,33 +135,11 @@ def add_coordinates(features, spatial_importance=1.0):
     return features
 
 
-def get_freqs(img):
-    """Compute the appropriate frequencies for an image of the given shape.
-
-    Frequencies are given in cycles/pixel.
-
-    """
-    n_cols = img.shape[1]
-    next_pow2 = 2 ** int(np.ceil(np.log2(n_cols)))
-    min_freq = next_pow2 / 4
-    n_freqs = int(np.log2(min_freq))
-
-    # note: paper gives frequency in cycles per image width.
-    # we need cycles per pixel, so divide by image width
-    frequencies =  list((np.sqrt(2) * float(2 ** i)) / n_cols
-                        for i in range(n_freqs))
-    return frequencies
-
-
-def segment_textures(img, model, freqs=None, thetas=None, n_thetas=4, select=True, k=4, coord=1):
+def segment_textures(img, model, freqs=None, angle=10, select=True, k=4, coord=1):
     """Segments textures using Gabor filters and k-means."""
-    if freqs is None:
-        freqs = get_freqs(img)[-4:]
-    if thetas is None:
-        thetas = np.deg2rad(np.arange(0, 180, 180.0 / n_thetas))
-    kernels, all_freqs = make_filter_bank(freqs, thetas)
-    filtered, all_freqs = filter_image(img, kernels, all_freqs, select=select)
-    features = compute_features(filtered, all_freqs)
+    filtered, kernel_freqs = filter_img(img, freqs, angle)
+    filtered, kernel_freqs = filter_selection(filtered, kernel_freqs)
+    features = compute_features(filtered, kernel_freqs)
     features = add_coordinates(features, spatial_importance=coord)
     n_feats = features.shape[-1]
     X = features.reshape(-1, n_feats)
@@ -147,33 +149,26 @@ def segment_textures(img, model, freqs=None, thetas=None, n_thetas=4, select=Tru
     return model.labels_.reshape(img.shape)
 
 
-def directionality_filter(img, freqs=None, thetas=None, n_thetas=18):
+def directionality_filter(filtered, angle=10):
     """
     Finds the maximum filter response for each pixel.
 
     Returns the maximum filter response and the angle of maximum response.
 
     """
-    if freqs is None:
-        freqs = get_freqs(img)[-4:]
-    if thetas is None:
-        thetas = np.deg2rad(np.arange(0, 180, 180.0 / n_thetas))
+    f2 = np.abs(filtered)
 
-    kernels, all_freqs = make_filter_bank(freqs, thetas)
-    filtered, all_freqs = filter_image(img, kernels, all_freqs, select=False)
-    f2 = np.power(filtered, 2)
-
-    n_thetas = len(thetas)
-    f2_angles = np.dstack(f2[:, :, i::n_thetas].sum(axis=2)
-                          for i in range(n_thetas))
+    n_angles = int(180 / angle)
+    f2_angles = np.dstack(f2[:, :, i::n_angles].sum(axis=2)
+                          for i in range(n_angles))
 
     max_angle_idx = np.argmax(f2_angles, axis=2)
     x, y = np.indices(max_angle_idx.shape)
     f2_maxes = f2[x, y, max_angle_idx]
     magnitude = f2_maxes / f2.mean(axis=2)
 
-    max_thetas = np.rad2deg(np.array(thetas))[max_angle_idx]
-    return magnitude, max_thetas
+    max_angles = np.rad2deg(np.array(f2_angles))[max_angle_idx]
+    return magnitude, max_angles
 
 
 def scale(arr):
