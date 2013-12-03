d9b8b45f0f4d12f4ade9b1361f8eea56b5d8c929
Kemal Eren <kemal@kemaleren.com>
2013-12-03 14:48:35 -0800

    save magnitude and angle images

--- hsv_all.py
+++ hsv_all.py
@@ -9,7 +9,9 @@ import texture as t
 ANGLE = 20
 
 filtered = os.path.expanduser("~/devel/results/filtered")
-results = os.path.expanduser("~/devel/results/hsv")
+hsv_results = os.path.expanduser("~/devel/results/hsv")
+magnitude_results = os.path.expanduser("~/devel/results/magnitude")
+angle_results = os.path.expanduser("~/devel/results/angle")
 
 filenames = list(f for f in os.listdir(filtered)
                  if os.path.splitext(f)[1] == ".npy")
@@ -17,14 +19,18 @@ n_files = len(filenames)
 
 for i, filename in enumerate(filenames):
     print "processing {} of {}".format(i, n_files)
-    img_file = os.path.splitext(filename)[0] + ".png"
-    img_path = os.path.join(results, img_file)
-
-    if os.path.exists(img_path):
-        continue
     arr = np.load(os.path.join(filtered, filename))
     magnitude, angle = t.directionality_filter(arr, angle=ANGLE)
-    img = hsv2rgb(t.make_hsv(magnitude, angle))
 
+    magnitude_file = os.path.splitext(filename)[0] + ".png"
+    magnitude_path = os.path.join(magnitude_results, magnitude_file)
+    pp.imsave(magnitude_path, magnitude, cmap=pp.cm.gray)
 
-    pp.imsave(img_path, img)
+    angle_file = os.path.splitext(filename)[0] + ".png"
+    angle_path = os.path.join(angle_results, angle_file)
+    pp.imsave(angle_path, angle)
+
+    hsv_file = os.path.splitext(filename)[0] + ".png"
+    hsv_path = os.path.join(hsv_results, hsv_file)
+    hsv = hsv2rgb(t.make_hsv(magnitude, angle))
+    pp.imsave(hsv_path, hsv)
